3429a19e7ada93b4cc79b83e8ce952d43352840b
AchyMake <achymake@gmail.com>
2024-02-05 07:52:54 +0100

    clean up

--- src/main/java/org/achymake/players/api/PlaceholderProvider.java
+++ src/main/java/org/achymake/players/api/PlaceholderProvider.java
@@ -19,7 +19,7 @@ public class PlaceholderProvider extends PlaceholderExpansion {
     }
     @Override
     public String getVersion() {
-        return "1.20.4";
+        return "1.10.3";
     }
     @Override
     public boolean canRegister() {
